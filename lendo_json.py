644898fc5a68cb5d772eb38b8796c179b6537248
Thiago Lima <thiagolimasilva689@gmail.com>
2026-07-26 11:46:36 -0400

    criando um novo estudo

---
+++ lendo_json.py
@@ -0,0 +1,4 @@
+import pandas as pd
+#historia = pd.read_csv(r'C:\Users\Thiago\OneDrive\Desktop\teiu\csv\titanic.csv')
+df_lido = pd.read_json(r'C:\Users\Thiago\OneDrive\Desktop\teiu\pandas_json\livros.json')
+print(df_lido)

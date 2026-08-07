4f129f3a60d7f7d485d20bc24c7e03ead36a3914
Thiago Lima <thiagolimasilva689@gmail.com>
2026-08-06 21:43:13 -0400

    dados novos

--- consultas_desenho.py
+++ consultas_desenho.py
@@ -1,3 +1,22 @@
 import pandas as pd
-df = pd.read_json(r'C:\Users\Thiago\OneDrive\Desktop\teiu\pandas_json\mato_grosso_sul.json')
-print(df.head())
+import json
+
+with open(r'C:\Users\Thiago\OneDrive\Desktop\teiu\pandas_json\rick_morty.json', 'r', encoding='utf-8') as f:
+    dados = json.load(f)
+
+df = pd.json_normalize(dados['results'])
+
+#Exercício 1 — Quantos personagens?
+#print(f"O TOTAL DE PERSONAGENS É : {len(df)}")
+#print(df[['id', 'name', 'status', 'species', 'gender']].head())
+
+
+#Exercício 2 — Filtrar vivos
+vivos = df[df['status'] == "Alive"]
+#print(f"A QUANTIDADE DE PERSONAGENS VIVOS É : {len(vivos)}")
+#print(vivos[['name', 'species']])
+
+#Exercício 3 — Agrupar por espécie
+especies = df.groupby('species')['id'].count().sort_values(ascending=False)
+print("Quantidade por espécie:")
+print(especies) 
